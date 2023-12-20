783253d89614867df88ac577ca4d614696d50453
binkrassdufass <binkrassdufass@gmail.com>
2023-12-20 10:42:52 +0100

    tst

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core')
-
+const github = require('@actions/github')
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
